0f2acc9bac0bd5519d014d8a1c4b48404fbd3c92
Lisa Olson <olson.lisa94@gmail.com>
2018-07-26 19:00:43 +0000

    updates permission

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { 'holberton':
     path    => '/tmp/holberton',
     ensure  => file,
-    mode    => '0700',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet'
