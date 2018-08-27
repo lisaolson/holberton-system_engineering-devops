a437dad7f7151e062fe674685488e92026eea807
Lisa Olson <olson.lisa94@gmail.com>
2018-08-27 23:33:13 +0000

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,8 @@ TOTAL_NUMBER_OF_TASKS = 0
 NUMBER_OF_DONE_TASKS = 0
 
 emp_id = int(sys.argv[1])
-resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(emp_id))
+resp = requests.\
+    get('https://jsonplaceholder.typicode.com/users/{}'.format(emp_id))
 data = resp.json()
 emp_name = data['name']
 
@@ -22,9 +23,8 @@ for todo in todo_data:
         if todo['completed']:
             NUMBER_OF_DONE_TASKS += 1
 
-EMPLOYEE_NAME = emp_name
-
-print('Employee {} is done with tasks({}/{}):'.format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+print('Employee {} is done with tasks({}/{}):'
+      .format(emp_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
 for todo in todo_data:
     if todo['userId'] == emp_id:
