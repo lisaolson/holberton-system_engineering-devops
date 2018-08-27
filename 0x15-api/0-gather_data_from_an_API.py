1e59f46fd0cc02acba85cf401acbb1a25fc8d1f6
Lisa Olson <olson.lisa94@gmail.com>
2018-08-27 23:40:39 +0000

    adds main guard

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,28 +5,32 @@ import requests
 import sys
 
 
-TASK_TITLE = ""
-TOTAL_NUMBER_OF_TASKS = 0
-NUMBER_OF_DONE_TASKS = 0
+def todo():
+    TASK_TITLE = ""
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
 
-emp_id = int(sys.argv[1])
-resp = requests.\
-    get('https://jsonplaceholder.typicode.com/users/{}'.format(emp_id))
-data = resp.json()
-emp_name = data['name']
+    emp_id = int(sys.argv[1])
+    resp = requests.\
+        get('https://jsonplaceholder.typicode.com/users/{}'.format(emp_id))
+    data = resp.json()
+    emp_name = data['name']
 
-todo_resp = requests.get('https://jsonplaceholder.typicode.com/todos')
-todo_data = todo_resp.json()
-for todo in todo_data:
-    if todo['userId'] == emp_id:
-        TOTAL_NUMBER_OF_TASKS += 1
-        if todo['completed']:
-            NUMBER_OF_DONE_TASKS += 1
+    todo_resp = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todo_data = todo_resp.json()
+    for todo in todo_data:
+        if todo['userId'] == emp_id:
+            TOTAL_NUMBER_OF_TASKS += 1
+            if todo['completed']:
+                NUMBER_OF_DONE_TASKS += 1
 
-print('Employee {} is done with tasks({}/{}):'
-      .format(emp_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(emp_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
-for todo in todo_data:
-    if todo['userId'] == emp_id:
-        if todo['completed']:
-            print('\t {}'.format(todo['title']))
+    for todo in todo_data:
+        if todo['userId'] == emp_id:
+            if todo['completed']:
+                print('\t {}'.format(todo['title']))
+
+if __name__ == '__main__':
+    todo()
