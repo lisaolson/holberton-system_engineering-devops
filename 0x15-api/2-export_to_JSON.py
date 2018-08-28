b7628d4ddd8f851daaedac8c22394a1697c754f4
Lisa Olson <olson.lisa94@gmail.com>
2018-08-28 02:41:43 +0000

    correct formatting, incorrect output

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# Gathers data from an API
+
+import json
+import requests
+import sys
+
+
+def export_json():
+    emp_id = int(sys.argv[1])
+    resp = requests.\
+        get('https://jsonplaceholder.typicode.com/users/{}'.format(emp_id)).\
+        json()
+    username = resp['username']
+    todo_resp = requests.\
+        get('https://jsonplaceholder.typicode.com/todos').json()
+
+    download_dir = "{}.json".format(sys.argv[1])
+
+    return_data = []
+    todo_dict = {}
+    with open(download_dir, "w", newline='') as jsonfile:
+        for todo in todo_resp:
+            if todo['userId'] == emp_id:
+                todo_dict['task'] = todo['title']
+                todo_dict['completed'] = todo['completed']
+                todo_dict['username'] = username
+                return_data.append(todo_dict)
+        return_dict = {emp_id: return_data}
+        json.dump(return_dict, jsonfile)
+
+if __name__ == '__main__':
+    export_json()
